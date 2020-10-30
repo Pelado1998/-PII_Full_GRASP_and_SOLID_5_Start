58d83748874014dc5aa3c5a612a61df0f2298d28
CristianVelazquez1995 <58026968+CristianVelazquez1995@users.noreply.github.com>
2020-10-30 17:30:02 -0300

    Agregado de justificacion

--- src/Library/IPrint.cs
+++ src/Library/IPrint.cs
@@ -1,6 +1,12 @@
 
 namespace Full_GRASP_And_SOLID
 {
+    /// <summary>
+    /// Se desea utilizar el patrón de diseño DIP ya que mediante este dieño se crea
+    /// flexibilidad y resistencia, ya que se pueden realizar cambios a diferentes clases,
+    /// pero sin modificar la clase ConsolePrinter, siempre y cuando esta ultima reciba un objeto
+    /// de tipo IPrint.
+    /// </summary>
     public interface IPrint
     {
         string GetTextToPrint();
